0babe532dd44f784f30a8569f26b38f8543741b5
TanyaMosk <tanya856227@gmail.com>
2023-08-27 18:01:03 +0200

    1

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,6 +1,13 @@
 import { Text } from 'components';
-import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
-import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+import {
+    TodoWrapper,
+    DeleteButton,
+    // EditButton
+} from './Todo.styled';
+import {
+    RiDeleteBinLine,
+    // RiEdit2Line
+} from 'react-icons/ri';
 
 export const Todo = () => {
     return (
