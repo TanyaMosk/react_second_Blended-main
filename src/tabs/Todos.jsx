6ce6f24aefb42cf0a63820f71aa03f65dba95045
TanyaMosk <tanya856227@gmail.com>
2023-08-27 17:13:05 +0200

    update

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,9 @@
-import { Component } from 'react';
-import { nanoid } from 'nanoid';
-import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
+// import { Component } from 'react';
+// import { nanoid } from 'nanoid';
+// import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
-export class Todos extends Component {
-  render() {
-    return <Text>Todos</Text>;
-  }
-}
+// export class Todos extends Component {
+//   render() {
+//     return <Text>Todos</Text>;
+//   }
+// }
