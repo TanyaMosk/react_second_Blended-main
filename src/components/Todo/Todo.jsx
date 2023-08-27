8ccadabcbfabf70eb48911fa861a34b80e4c6446
TanyaMosk <tanya856227@gmail.com>
2023-08-27 17:58:37 +0200

    update

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,5 +3,19 @@ import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
 export const Todo = () => {
-  return <h2>Todo</h2>;
+    return (
+    <>
+      <TodoWrapper>
+       <Text textAlign="center" marginBottom="20px">
+         TODO #1
+        </Text>
+        <Text>Some description</Text>
+        <DeleteButton type="button">
+        <RiDeleteBinLine size={24} />
+        </DeleteButton>
+      </TodoWrapper>
+    </>
+    )
+  
+    // <h2>Todo</h2>;
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,22 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+    state = {
+        todos: [], 
+    }
+
   render() {
-    return <Text>Todos</Text>;
+      return (
+          <>
+       <Text>Todos</Text>
+        <SearchForm />
+         <Grid>
+            <GridItem>
+              <Todo />
+            </GridItem>
+         </Grid>
+        </>
+      )
+      
   }
 }
